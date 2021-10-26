0b42b891724abe0c0a2a8ccc06862b0a116939ee
SilverMoon23 <sashakucar200215@gmail.com>
2021-10-26 10:26:51 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const addTask = document.querySelector('.add__task');
+    const addTask = document.querySelector('.add__task'),
+          taskList = document.querySelector('.task__list'),
+          tasks = document.querySelectorAll('.task'),
+          taskInput = document.querySelector('.task__input'),
+          saveBtn = document.querySelector('.save__btn'),
+          cancelBtn = document.querySelector('.cancel__btn');
+
 
     addTask.addEventListener('click', () => {
-        console.log('click');
+        if (taskInput.classList.contains('hide')) {
+            taskInput.classList.remove('hide');
+            taskInput.classList.add('show');
+        }
+    });
+    cancelBtn.addEventListener('click', () => {
+        if (taskInput.classList.contains('show')) {
+            taskInput.classList.remove('show');
+            taskInput.classList.add('hide');
+        }
     });
 });
+
