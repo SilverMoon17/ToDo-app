eff9f06f37b6b4cf9eed450948846596912cd451
SilverMoon23 <sashakucar200215@gmail.com>
2021-11-07 18:05:37 +0200

    Added functionality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', (e) => {
     const addTask = document.querySelector('.add__task'),
           taskList = document.querySelector('.task__list'),
           tasks = document.querySelectorAll('.task'),
           taskInput = document.querySelector('.task__input'),
           saveBtn = document.querySelector('.save__btn'),
-          cancelBtn = document.querySelector('.cancel__btn');
+          cancelBtn = document.querySelector('.cancel__btn'),
+          newTaskInput = document.querySelector('.new__task');
+
+    function createTask(task) {
+        const li = document.createElement('li');
+        li.classList.add('task');
+        li.innerHTML = `
+        <img src="img/notDone.svg" class="icon" alt="">
+        <p class="task__text">${task}</p>
+        `;
+        taskList.prepend(li);
+    }
+
+    document.addEventListener('keyup', (e) => {
+        if (taskInput.classList.contains('show') && e.key == 'Enter' && newTaskInput.value !== '') {
+            createTask(newTaskInput.value);
+            newTaskInput.value = '';
+        } else if (e.key == 'Escape' && taskInput.classList.contains('show')) {
+            taskInput.classList.remove('show');
+            taskInput.classList.add('hide');
+            newTaskInput.value = '';
+        }
+    });
 
 
     addTask.addEventListener('click', () => {
@@ -12,6 +34,10 @@ window.addEventListener('DOMContentLoaded', () => {
             taskInput.classList.remove('hide');
             taskInput.classList.add('show');
         }
+    });
+    saveBtn.addEventListener('click', () => {
+        createTask(newTaskInput.value);
+        newTaskInput.value = '';
     });
     cancelBtn.addEventListener('click', () => {
         if (taskInput.classList.contains('show')) {
