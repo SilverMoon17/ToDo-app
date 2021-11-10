19b02977022966595f5fe7f39ee4ad4dce6bcb2a
SilverMoon23 <sashakucar200215@gmail.com>
2021-11-10 10:29:55 +0200

    v1.0

--- js/script.js
+++ js/script.js
@@ -6,23 +6,39 @@ window.addEventListener('DOMContentLoaded', (e) => {
           saveBtn = document.querySelector('.save__btn'),
           cancelBtn = document.querySelector('.cancel__btn'),
           newTaskInput = document.querySelector('.new__task'),
-          icons = document.querySelectorAll('.icon');
-          
+          icons = document.querySelectorAll('.icon'),
+          emptyListTitle = document.querySelector('.empty__list-title');
 
-          
-    function createTask(task) {
-        const li = document.createElement('li');
-        li.classList.add('task', 'not__done');
-        li.innerHTML = `
-        <img src="img/notDone.svg" class="icon" alt="">
-        <p class="task__text">${task}</p>
-        `;
-        taskList.prepend(li);
-    }
+    let tasksDB = {
+        tasks: []
+    };
+
+    tasksDB = JSON.parse(localStorage.getItem('tasksDB')) || tasksDB;
+
+    const createTask = (task) => {
+        console.log(task);
+        tasksDB.tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasksDB));
+    };
+
+    const createTaskList = (tasks, parent) => {
+        parent.innerHTML = '';
+
+        tasks.forEach(task => {
+            const li = document.createElement('li');
+            li.classList.add('task', 'not__done');
+            li.innerHTML = `
+            <img src="img/notDone.svg" class="icon" alt="">
+            <p class="task__text">${task}</p>
+            `;
+            parent.append(li);
+        });
+    };
 
     document.addEventListener('keyup', (e) => {
         if (taskInput.classList.contains('show') && e.key == 'Enter' && newTaskInput.value !== '') {
             createTask(newTaskInput.value);
+            createTaskList(tasksDB.tasks, taskList);
             newTaskInput.value = '';
         } else if (e.key == 'Escape' && taskInput.classList.contains('show')) {
             taskInput.classList.remove('show');
@@ -30,25 +46,6 @@ window.addEventListener('DOMContentLoaded', (e) => {
             newTaskInput.value = '';
         }
     });
-
-    // tasks.forEach((task) => {
-    //     task.addEventListener('mouseenter', (e) => {
-    //         const target = e.target;
-    //        if (target.classList.contains('not__done')) {
-    //            target.children[0].src = 'img/done.svg';
-    //        }
-    //     });
-    //     task.addEventListener('mouseleave', (e) => {
-    //         const target = e.target;
-    //        if (target.classList.contains('not__done')) {
-    //            target.children[0].src = 'img/notDone.svg';
-    //        }
-    //     });
-    // });
-
-    // делегирование событий в списке задач 
-    
-    
 
     taskList.addEventListener('mouseenter', e => {
         const target = e.target;    
@@ -76,6 +73,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
     addTask.addEventListener('click', () => {
         if (taskInput.classList.contains('hide')) {
+            emptyListTitle.classList.add('hide');
             taskInput.classList.remove('hide');
             taskInput.classList.add('show');
             newTaskInput.focus();
@@ -91,4 +89,13 @@ window.addEventListener('DOMContentLoaded', (e) => {
             taskInput.classList.add('hide');
         }
     });
+
+    if (localStorage.getItem('tasks')) {
+        emptyListTitle.classList.add('hide');
+        tasksDB = JSON.parse(localStorage.getItem('tasks'));
+        createTaskList(tasksDB.tasks, taskList);
+    } else {
+        emptyListTitle.classList.add('show');
+    }
+        
 });
