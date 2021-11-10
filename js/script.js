1c3ad4a565b8bd6fa99318341fc0bec33175e062
SilverMoon23 <sashakucar200215@gmail.com>
2021-11-10 09:30:16 +0200

    New functionality

--- js/script.js
+++ js/script.js
@@ -5,11 +5,14 @@ window.addEventListener('DOMContentLoaded', (e) => {
           taskInput = document.querySelector('.task__input'),
           saveBtn = document.querySelector('.save__btn'),
           cancelBtn = document.querySelector('.cancel__btn'),
-          newTaskInput = document.querySelector('.new__task');
+          newTaskInput = document.querySelector('.new__task'),
+          icons = document.querySelectorAll('.icon');
+          
 
+          
     function createTask(task) {
         const li = document.createElement('li');
-        li.classList.add('task');
+        li.classList.add('task', 'not__done');
         li.innerHTML = `
         <img src="img/notDone.svg" class="icon" alt="">
         <p class="task__text">${task}</p>
@@ -28,11 +31,54 @@ window.addEventListener('DOMContentLoaded', (e) => {
         }
     });
 
+    // tasks.forEach((task) => {
+    //     task.addEventListener('mouseenter', (e) => {
+    //         const target = e.target;
+    //        if (target.classList.contains('not__done')) {
+    //            target.children[0].src = 'img/done.svg';
+    //        }
+    //     });
+    //     task.addEventListener('mouseleave', (e) => {
+    //         const target = e.target;
+    //        if (target.classList.contains('not__done')) {
+    //            target.children[0].src = 'img/notDone.svg';
+    //        }
+    //     });
+    // });
+
+    // делегирование событий в списке задач 
+    
+    
+
+    taskList.addEventListener('mouseenter', e => {
+        const target = e.target;    
+        if (target.classList.contains('not__done')) {
+            target.children[0].src = 'img/done.svg';
+            target.classList.add('hover');
+        }
+    },true);
+
+    taskList.addEventListener('click', e => {
+        const target = e.target;
+        console.log(target);
+        if (target && target.classList.contains('icon')) {
+            target.parentElement.remove();
+        }
+    });
+
+    taskList.addEventListener('mouseleave', e => {
+        const target = e.target;    
+        if (target.classList.contains('hover')) {
+            target.children[0].src = 'img/notDone.svg';
+            target.classList.remove('hover');
+        }
+    },true);
 
     addTask.addEventListener('click', () => {
         if (taskInput.classList.contains('hide')) {
             taskInput.classList.remove('hide');
             taskInput.classList.add('show');
+            newTaskInput.focus();
         }
     });
     saveBtn.addEventListener('click', () => {
@@ -46,4 +92,3 @@ window.addEventListener('DOMContentLoaded', (e) => {
         }
     });
 });
-
